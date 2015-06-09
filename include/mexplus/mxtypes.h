c96a549347bfde8b205a337aff0006acd0ae1d1b
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2015-06-09 09:23:32 +0900

    Added assignment operator

--- include/mexplus/mxtypes.h
+++ include/mexplus/mxtypes.h
@@ -1,6 +1,6 @@
 /** MxTypes and other type traits for template.
  *
- * Kota Yamaguchi 2014 <kyamagu@cs.stonybrook.edu>
+ * Kota Yamaguchi 2014  http://github.com/kyamamgu/mexplus
  */
 
 #ifndef __MEXPLUS_MXTYPES_H__
